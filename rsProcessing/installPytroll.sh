154a1ea9488f3494750132224f962fdedf4f4f9a
Tom Parker <tparker@usgs.gov>
2016-08-04 23:17:50 +0000

    continue initial setup

--- rsProcessing/installPytroll.sh
+++ rsProcessing/installPytroll.sh
@@ -2,24 +2,33 @@
 set -x
 
 export GIT_SSL_NO_VERIFY=true
-#for i in pytroll-schedule pyresample mpop posttroll pycoast pydecorate pyorbital trollduction ; do
-for i in pyresample mpop posttroll pycoast pydecorate pyorbital pytroll-schedule; do
+
+
+
+
+for i in pyresample mpop posttroll pycoast pydecorate pyorbital pytroll-schedule pyspectral python-geotiepoints trollduction; do
 	echo "+++ installing $i"
 	git clone --progress --verbose git://github.com/pytroll/${i}.git
 	cd $i
 	if [ $i = "posttroll" ] ; then
-		echo "+++ switching to branch develop"
+		git checkout develop
+	elif [ $i = "pyresample" ] ; then
+		git checkout pre-master
+	elif [ $i = "pyorbital" ] ; then
 		git checkout develop
 	elif [ $i = "pytroll-schedule" ] ; then
-		echo "+++ switching to branch develop"
-		git branch
 		git checkout develop
-		git branch
+	elif [ $i = "pycoast" ] ; then
+		git checkout pre-master
+	elif [ $i = "python-geotiepoints" ] ; then
+		git checkout develop
 	fi
 	python setup.py install
 	cd ..
 done
 
-git clone git://github.com/tparker-usgs/trollduction.git
-cd trollduction
-python setup.py install
+
+#git clone git://github.com/tparker-usgs/trollduction.git
+#git clone git://github.com/pytroll/trollduction.git
+#cd trollduction
+#python setup.py install
