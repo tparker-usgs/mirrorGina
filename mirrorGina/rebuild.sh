837f4b71bf626a6b3044628bbe52985f0d773fa5
Tom Parker <tparker@usgs.gov>
2016-10-01 01:13:13 +0000

    tweak cron path

--- mirrorGina/rebuild.sh
+++ mirrorGina/rebuild.sh
@@ -3,8 +3,8 @@
 # cron is fussy about this
 chmod 644 cron-mirrorGina
 
-systemctl stop mirrorGina.service && \
-docker stop mirrorgina && \
-docker rm mirrorgina && \
+#systemctl stop mirrorGina.service && \
+docker stop mirrorgina 
+docker rm mirrorgina 
 docker build -t mirrorgina . && \
 docker run --detach=true --volumes-from data --name mirrorgina mirrorgina
