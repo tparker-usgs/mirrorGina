d0767c6fa9dc0cd45ae206d57553de02b36829bf
Tom Parker <tparker@usgs.gov>
2017-05-16 20:21:37 +0000

    inital config cleanup

--- trollduction/rebuild.sh
+++ trollduction/rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #systemctl stop trollduction.service || docker stop trollduction
+docker build -t trollduction . 
 docker stop trollduction
 docker rm trollduction 
-docker build -t trollduction . && \
 docker run --detach=true --volumes-from data --name trollduction trollduction
