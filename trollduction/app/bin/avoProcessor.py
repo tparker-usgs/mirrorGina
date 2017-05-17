13625059c01873a581c3955240efa1ca332835cc
Tom Parker <tparker@usgs.gov>
2017-05-17 00:02:02 +0000

    trolldutcion tweaks

--- trollduction/app/bin/avoProcessor.py
+++ trollduction/app/bin/avoProcessor.py
@@ -5,8 +5,8 @@ from posttroll.subscriber import Subscribe
 from posttroll.message import datetime_encoder
 from pprint import pprint
 from mpop.satellites import PolarFactory
-import datetime
-import dateutil.parser
+from datetime import timedelta, datetime
+from dateutil import parser
 from mpop.utils import debug_on
 from trollsched.satpass import Pass
 from mpop.projector import get_area_def
@@ -26,7 +26,7 @@ from mpop.projector import get_area_def
 #             return {"val": str(obj), "_spec_type": "decimal"}
 #         else:
 #             return super().default(obj)
-
+#
 # def object_hook(obj):
 #     _spec_type = obj.get('_spec_type')
 #     if not _spec_type:
@@ -37,25 +37,29 @@ from mpop.projector import get_area_def
 #     else:
 #         raise Exception('Unknown {}'.format(_spec_type))
 
-
+ORBIT_SLACK = timedelta(minutes=30)
 def main():
     with Subscribe('', "pytroll://ir108-EARS/Suomi-NPP/viirs/1b", True) as sub:
         for msg in sub.recv():
             datas = json.dumps(msg.data, default=datetime_encoder)
             data = json.loads(datas)
             pprint(data)
-            # platform_name = data["platform_name"]
+            platform_name = data["platform_name"]
             # orbit = data["orbit_number"]
-            start_date = dateutil.parser.parse(data["start_date"])
-            start_time = dateutil.parser.parse(data["collection"][0]["start_time"])
-            start_time = datetime.datetime.combine(start_date, start_time.time())
+            start_date = parser.parse(data["start_date"])
+            print "START: %s" % start_date
+            #start = start_date - ORBIT_SLACK
+            #end = start_date + ORBIT_SLACK
+            start = start_date
+            end = start_date + timedelta(minutes=1)
+            print ("start %s :: %s" % (start, type(start)))
+            print ("end %s :: %s" % (end, type(end)))
+            # print "END: %s" % end_time)
+            overpass = Pass(platform_name, start, end, instrument='viirs')
+            coverage = overpass.area_coverage(get_area_def("AKSC")) 
+            #coverage = overpass.area_coverage(get_area_def("AKSC")) * 100
 
-            print "START: %s" + str(start_time)
-            print 'START: %s' % type(start_time)
-            # print "END: " + str(end_time) + "\n"
-            # overpass = Pass(platform_name, start_time, end_time)
-            # coverage = overpass.area_coverage(get_area_def("AKSC")) * 100
-            # print "COVERAGE: " + str(coverage) + "%\n"
+            #print "COVERAGE: %s" % coverage
             #
             # global_data = PolarFactory.create_scene("Suomi-NPP", "", "viirs", end_time, orbit)
             # global_data.load(["M15"], time_interval=(start_time, end_time))
