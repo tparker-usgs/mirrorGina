eecc92aae4d3ce15731fd9121408df5197985674
Tom Parker <tparker@usgs.gov>
2017-06-29 15:19:21 -0800

    tweak omps mirror script

--- collectors/bin/mirrorGinaOmps.sh
+++ collectors/bin/mirrorGinaOmps.sh
@@ -1,4 +1,4 @@
-/bin/sh
+#!/bin/sh
 
 cd /data/omps/edr
 wget -nd -r -l 1 --timeout 20 -nc -np --accept-regex='OMPS-NPP-TC_EDR_SO2NRT-*' -e robots=off http://dds.gina.alaska.edu/public/IPOPP/npp/omps/level2/
